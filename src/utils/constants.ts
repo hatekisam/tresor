f62d16e9bf4673e633c2026dab1d905b700f7265
mugishayves <mugishayves189000@gmail.com>
2023-09-23 22:58:36 +0200

    fixed the post of login

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
-// export const backend = "http://194.163.167.131:8050"
-export const backend = "http://localhost:3000";
+export const backend = "http://194.163.167.131:8050"
+// export const backend = "http://localhost:3000";
